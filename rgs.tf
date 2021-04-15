0f724cad190be7453f9011c754fab72a7aec3d10
kealeyg <66959582+kealeyg@users.noreply.github.com>
2021-04-15 19:57:27 -0400

    updates

--- rgs.tf
+++ rgs.tf
@@ -1,14 +1,14 @@
 /* Create RG
 ------------------------------------------------------------------*/
 resource "azurerm_resource_group" "system-rg" {
-    name     = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"_","System","-","rg"])
+    name = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"_","System","-","rg"])
     location = "canadaCentral"
     provider = azurerm.sub
     tags = var.globals.tags
 }
 
 resource "azurerm_resource_group" "network-rg" {
-    name     = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"_","Network","-","rg"])
+    name = join("", [var.globals.env,"-",var.globals.group,"-",var.globals.project,"_","Network","-","rg"])
     location = "canadaCentral"
     provider = azurerm.sub
     tags = var.globals.tags
